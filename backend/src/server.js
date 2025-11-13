3ef828bfdb3c049dc5433d7e3e7d0a703dd1bb1e
Sameer-ts-tech <sameer.ts.tech@gmail.com>
2025-11-13 15:04:31 +0530

    database-setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,42 @@
 import express from "express";
+import path from "path"
 import { ENV } from "./lib/env.js";
+import { connectDB } from "./lib/db.js";
 
 const app=express()
 
-console.log(ENV.PORT);
-console.log(ENV.DB_URL);
+const __dirname = path.resolve()
 
 
-app.get("/",(req,res)=>{
-    res.status(200).json({msg:"success from api"})
+app.get("/health",(req,res)=>{
+    res.status(200).json({msg:"api is up and running"})
+})
+app.get("/work",(req,res)=>{
+    res.status(200).json({msg:"api is working"})
 })
 
-app.listen(ENV.PORT,()=>{
-    console.log("Server is running on port:",ENV.PORT);
-    
-})
+//make app ready for deployment
+
+if(ENV.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+    app.get("/{*any}",(req,res)=>{
+        //any error for checking
+        res.sendFile(path.join(__dirname,"../frontend/dist/index.html"));
+    })
+}
+
+
+
+const startServer =async ()=>{
+    try {
+        await connectDB();
+        app.listen(ENV.PORT,()=>{
+        console.log("Server is running on port:",ENV.PORT);
+    })
+        
+    } catch (error) {
+        console.error("Error starting the server",error);
+        
+    }
+}
+startServer();
